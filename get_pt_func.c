5f986e3b1d0ca1d43c9b944310c11925fbe44a98
Tsim <tyang4204@gmail.com>
2023-11-06 15:43:08 -0800

    update

--- get_pt_func.c
+++ get_pt_func.c
@@ -6,32 +6,4 @@
  */
 char (*get_pt_func(char *s))(char)
 {
-	pt_f pts[] = {
-		{"c", pt_character},
-		{NULL,NULL}};
-	int i = 0;
-
-	while (i < 3)
-	{
-		if (_strcmp(s, pts[i].c) == 0)
-		{
-			return (pts[i].p);
-		}
-	i++;
-	}
-	return (0);
-}
-
-int _strcmp(char *s1, char *s2)
-{
-  int loop;
-  while (s1[loop] != '\0')
-    {
-      if (s1[loop] == s2[loop])
-	{
-	  return (0);
-	}
-      loop++;
-    }
-  return (1);
-}
+  
