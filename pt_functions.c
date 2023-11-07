010fcb14ee2c9ba6e942dc544bc37db86c9ce5c6
Tsim <tyang4204@gmail.com>
2023-11-07 12:45:02 -0800

    nothing was done

--- pt_functions.c
+++ pt_functions.c
@@ -5,11 +5,11 @@
 */
 int pt_character(va_list args)
 {
-  char temp = va_arg(args, int);
-  if (!temp)
-    {
-      return (-1);
-    }
-  _putchar(temp);
-  return (1);
+char temp = va_arg(args, int);
+if (!temp)
+{
+return (-1);
+}
+_putchar(temp);
+return (1);
 }
