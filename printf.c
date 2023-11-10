575a888c92ddc8e4e7b18396b671a589ad1712ee
Tsim <tyang4204@gmail.com>
2023-11-10 05:39:32 -0800

    betty proofed printf file

--- printf.c
+++ printf.c
@@ -2,35 +2,36 @@
 #include <stdio.h>
 /**
  * _printf - printing function c
+ * @format: string to hole format specifyer and characters
  * Return: input
-*/
+ */
 int _printf(const char *format, ...)
 {
-  int (*point)(va_list args);
-  int i = 0;
-  va_list args;
-  va_start(args, format);
-  if (*format != '\0')
-    {
-      while (*format != '\0')
-	{
-	  if (*format == '%')
-	    {
-	      format++;
-	      point = get_pt_func(*format);
-	      if (point == NULL)
-		{
-		  return (-1);
-		}
-	      i += point(args);
-	    }
-	  else
-	    {
-	      _putchar(*format);
-	    }
-	  format++;
-	}
-    }
-  va_end(args);
-  return(i);
+int (*point)(va_list args);
+int i = 0;
+va_list args;
+va_start(args, format);
+if (*format != '\0')
+{
+while (*format != '\0')
+{
+if (*format == '%')
+{
+format++;
+point = get_pt_func(*format);
+if (point == NULL)
+{
+return (-1);
+}
+i += point(args);
+}
+else
+{
+_putchar(*format);
+}
+format++;
+}
+}
+va_end(args);
+return (i);
 }
